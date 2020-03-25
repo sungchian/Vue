5d31b196716ecf6d2c22b2fab4506b750c7e0a36
sungchian <bn870829@gmail.com>
2020-03-25 11:22:02 +0800

    second

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,10 @@
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div id="nav">
+     <router-link :to="{ name: 'home' }">Vue</router-link> |
+     <router-link :to="{ name: 'messages' }">Django Rest</router-link>
+    </div>
   </div>
 </template>
 
